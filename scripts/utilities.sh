e267e8541a08f8755fcd8642aae26807873033d6
Murilo Cremon <murilo.ccoutinho@gmail.com>
2025-09-17 16:39:58 -0300

    Added X11 installation for full screen resolution

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -6,5 +6,6 @@ utilities_installation () {
     log -i "===> Installing Utilities"
     sudo dnf install -y git
     sudo dnf install -y vim
+    sudo dnf install -y plasma-workspace-x11 # Fix fullscreen bug in KDE
     log -s "===> Successfully Installed Utilities"
 }
